f200a59c72ea93a376871e5c310966b9b7de4b02
Iluveu <86732324+Iluveu@users.noreply.github.com>
2021-06-30 22:40:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `Nếu có 2 chồng sách,
     1 chồng nhỏ 1 chồng to.
     Thì tớ sẽ làm chồng nhỏ
-    còn cậu là chong to ♥️ `
+    còn cậu là chong to ♥️ `,
     btnIntro: 'Ok hông này',
     title: ' Cậu có thích tớ hông nà? ',
     desc: ' Có đúng hông đúng hông :33???',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: ' I love u too ✨ ',
     messDesc: ' Đùa tý thôi đừng đấm em (｡ﾉω＼｡)',
     btnAccept: 'Send help ',
-    messLink: 'https://m.me/123kalico2 '
+    messLink: 'https://m.me/123kalico2 ',
 }
